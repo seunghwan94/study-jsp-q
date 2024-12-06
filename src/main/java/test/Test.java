5ea051f24883fc8ee09fb31e719d8c33143cf684
DH.M <seunghwan94@gmail.com>
2024-12-06 21:55:31 +0900

    'first'

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Test extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/test.jsp").forward(req, resp);
+	}
+	
+}
